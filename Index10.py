a4b2f1f0cbac046f4454556c46c5ac6fa93515d7
Rustambek2003 <rustambekjumaqulov@gmail.com>
2022-10-23 21:23:07 +0500

    10

--- Index10.py
+++ Index10.py
@@ -6,4 +6,15 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    ans = 0
+    if type(int(s[0])) == int:
+        ans += int(s[0])
+    if type(int(s[1])) == int:
+        ans += int(s[1])
+    if type(int(s[2])) == int:
+        ans += int(s[2])
+    if type(int(s[3])) == int:
+        ans += int(s[3])
+    if type(int(s[4])) == int:
+        ans += int(s[4])
+    return ans
