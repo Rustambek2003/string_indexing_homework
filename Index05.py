1cb1376de219e1963acba24234d83e1799101cac
Rustambek2003 <rustambekjumaqulov@gmail.com>
2022-10-24 10:00:30 +0500

    5

--- Index05.py
+++ Index05.py
@@ -7,15 +7,15 @@ def main(s):
         int: answer
     """
     ans = 0
-    if type(int(s[0])) == int:
+    if s[0]=='0' or s[0]=='1' or s[0]=='2' or s[0]=='3' or s[0]=='4' or s[0]=='5' or s[0]=='6' or s[0]=='7' or s[0]=='8' or s[0]=='9':
         ans += 1
-    if type(int(s[0])) == int:
+    if s[1]=='0' or s[1]=='1' or s[1]=='2' or s[1]=='3' or s[1]=='4' or s[1]=='5' or s[1]=='6' or s[1]=='7' or s[1]=='8' or s[1]=='9':
+       ans += 1
+    if s[2]=='0' or s[2]=='1' or s[2]=='2' or s[2]=='3' or s[2]=='4' or s[2]=='5' or s[2]=='6' or s[2]=='7' or s[2]=='8' or s[2]=='9':
         ans += 1
-    if type(int(s[0])) == int:
+    if s[3]=='0' or s[3]=='1' or s[3]=='2' or s[3]=='3' or s[3]=='4' or s[3]=='5' or s[3]=='6' or s[3]=='7' or s[3]=='8' or s[3]=='9':
         ans += 1
-    if type(int(s[0])) == int:
-        ans += 1
-    if type(int(s[0])) == int:
+    if s[4]=='0' or s[4]=='1' or s[4]=='2' or s[4]=='3' or s[4]=='4' or s[4]=='5' or s[4]=='6' or s[4]=='7' or s[4]=='8' or s[4]=='9':
         ans += 1
     return ans
 print(main("32x3z"))
